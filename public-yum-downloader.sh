90d000b9ad829b74e2a5661c2f62a58d2524d956
Alvaro Miranda <kikitux@gmail.com>
2013-03-02 09:49:41 +1300

    added public-yum.repo for lxc clients
    added public-yum.repo for lxc clients

--- public-yum-downloader.sh
+++ public-yum-downloader.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# 201302250000
+# 201303020000
 # public-yum-downloader.sh
 #
 # public-yum-downloader script, to download a yum repository
@@ -85,11 +85,19 @@ repo_create()
         else
             die "Unsupported release $container_release_major"
         fi
-
-        wget -N -q $public_url/$repofile -O $tmpdir/$repofile    
+        
+        #repofile in tmp will be used to download the packages
+        wget -N -q $public_url/$repofile -O $tmpdir/$repofile
         if [ $? -ne 0 ]; then
             die "Failed to download repo file $public_url/$repofile"
         fi
+
+        #repofile in container_rootfs will be there for lxc clients
+        wget -N -q $public_url/$repofile -O $container_rootfs/$repofile
+        if [ $? -ne 0 ]; then
+            die "Failed to download repo file $container_rootfs/$repofile
+        fi
+        
         wget -N -q $public_url/$gpgkeyfile -O $container_rootfs/$gpgkeyfile
         if [ $? -ne 0 ]; then
             die "Failed to download gpg-key file $public_url/$gpgkeyfile"
